1edc88441f6e34d9a4517774dfc6cff683590e3d
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-29 17:40:19 +0200

    environment configuration

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+const app = express();
 //dotenv configuration starts
 
-dotenv.config()
+dotenv.config();
 
-
+const DBC_URL = process.env.DBC;
+const PORT = process.env.PORT || 3000;
 
 //---------------------ends
+
+//--------- global routes
